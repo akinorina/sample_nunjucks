0af86cec88beb6275eb391bcd47df827afe4280e
Akinori Nakata <nakata@real-its.com>
2021-12-03 15:57:29 +0900

    nunjucks テンプレート用にコードを変更

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var nunjucks = require('nunjucks');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,8 +11,12 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'twig');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
